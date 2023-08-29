2175bd9c27befdb3c966ca5c64ab9af9bae2c76f
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-08-28 18:16:47 -0600

    NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,13 @@
 import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quia necessitatibus sunt exercitationem iusto laboriosam vel animi corrupti a in, quis quasi reprehenderit debitis quod fugiat aut repellat officiis cum.</Typography> */}
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   );
 };
