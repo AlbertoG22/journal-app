2dd037577e1011cd0cd4e85f72d6f5dfd2fd7ff1
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-09-26 13:37:47 -0600

    .env variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,12 +7,18 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWpCm8Ak6eXWfeNWqcPrdryD9XVzLRHOQ",
-  authDomain: "react-course-fherrera.firebaseapp.com",
-  projectId: "react-course-fherrera",
-  storageBucket: "react-course-fherrera.appspot.com",
-  messagingSenderId: "295838913362",
-  appId: "1:295838913362:web:5fdf127db863b2014a608b"
+  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_APP_ID
+  // apiKey: "AIzaSyBWpCm8Ak6eXWfeNWqcPrdryD9XVzLRHOQ",
+  // authDomain: "react-course-fherrera.firebaseapp.com",
+  // projectId: "react-course-fherrera",
+  // storageBucket: "react-course-fherrera.appspot.com",
+  // messagingSenderId: "295838913362",
+  // appId: "1:295838913362:web:5fdf127db863b2014a608b"
 };
 
 // Initialize Firebase
