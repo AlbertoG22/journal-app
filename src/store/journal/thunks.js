286b67947b0b2917fe90587239f0453cb1f7bfe4
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-09-28 14:25:26 -0600

    store new note in Firebase

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -3,6 +3,15 @@ import { FirebaseDB } from '../../firebase/config';
 
 export const startNewNote = () => {
     return async( dispatch, getState ) => {
+        const { uid } = getState().auth;
 
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        };
+
+        const newDoc = doc( collection( FirebaseDB, `${uid}/journal/notes`) );
+        await setDoc( newDoc, newNote );
     };
 };
