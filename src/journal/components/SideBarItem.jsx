bc107771f40619a173b3c59e906923bbc77a7723
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-09-28 20:06:47 -0600

    set active note when clicking on a note

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,22 +1,29 @@
-import { useMemo } from "react";
+import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   Grid,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
-} from "@mui/material";
-import { TurnedInNot } from "@mui/icons-material";
+} from '@mui/material';
+import { TurnedInNot } from '@mui/icons-material';
+import { setActiveNote } from '../../store/journal';
 
-export const SideBarItem = ({ title, body, id }) => {
+export const SideBarItem = ({ title, body, id, date, imageUrls = [] }) => {
   // por si el título es muy largo, que se corte y se muestren los ...
   const newTitle = useMemo(() => {
     return title.length > 17 ? title.substring(0, 17) + "..." : title;
   }, [title]);
+  const dispatch = useDispatch();
+
+  const onClickNote = () => {
+    dispatch( setActiveNote({ title, body, id, date, imageUrls }) );
+  };
 
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={onClickNote}>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
